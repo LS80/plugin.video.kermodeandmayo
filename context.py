50238c2e768463911578d7d58b40d80819b964c4
LS80 <github@lee-smith.me.uk>
2016-03-27 22:37:38 +0100

    Ignore any label formatting for context menu YouTube search

--- context.py
+++ context.py
@@ -3,6 +3,8 @@ import re
 import xbmc
 
 if __name__ == '__main__':
-    movie_title = re.sub(' +', '+', sys.listitem.getLabel())
+    movie_title = sys.listitem.getLabel()
+    movie_title = re.sub('\[.*?]', '', movie_title)
+    movie_title = re.sub(' +', '+', movie_title)
     path = "plugin://plugin.video.kermodeandmayo/youtube/search/{0}".format(movie_title)
     xbmc.executebuiltin("Container.Update({0})".format(path))
